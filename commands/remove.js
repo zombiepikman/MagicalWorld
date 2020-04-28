e534f383557044905224b3f6e52c8fed9f8663c8
zombiepikman <37503132+zombiepikman@users.noreply.github.com>
2020-04-28 14:42:18 +0200

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -2,7 +2,20 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-var user =  message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+    const categoryId = "643519706640416787";
+
+    if (message.channel.parentID == categoryId) {
+  
+      message.channel.delete();
+  
+    } else {
+  
+      message.channel.send("Dit kan alleen in ticket kanaal!");
+   
+  
+    };
+
+var user2 = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
 
 if(!message.member.permissions.has("KICK_MEMBERS")) return message.channel.send("Hier heb jij **geen** rechten voor!");
 
@@ -10,7 +23,7 @@ if(args[0] == null) return message.channel.send("Use: !remove (user)");
 
 if(user.permissions.has("KICK_MEMBERS")) return message.channel.send("Deze gebruiker kan je niet van de ticker verwijderen.");
 
-user.updateOverwrite(user, {
+channel.updateOverwrite(user2, {
     "SEND_MESSAGES": true, "VIEW_CHANNEL": true,
     "ATTACH_FILES": true
 });
