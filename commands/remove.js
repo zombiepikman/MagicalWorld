7478cf1d20675e06c9e74c342d7210b95c61de66
zombiepikman <37503132+zombiepikman@users.noreply.github.com>
2020-04-28 10:46:34 +0200

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
- if(!message.member.hasPermissiosn("KICK_MEMBERS")) return message.channel.send("Hier heb jij **geen** rechten voor!");
+ if(!message.member.hasPermissions("KICK_MEMBERS")) return message.channel.send("Hier heb jij **geen** rechten voor!");
 
  if(args[0] == null) return message.channel.send("Gebruik: !remove (user)");
 
