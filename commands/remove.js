415d372e7e79ffd5162cb15adcd6464a2dc6fa55
zombiepikman <37503132+zombiepikman@users.noreply.github.com>
2020-04-28 14:48:37 +0200

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -15,7 +15,7 @@ module.exports.run = async (bot, message, args) => {
   
     };
 
-var user2 = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+var user2 = message.guild.members.cache.get(args[0]);
 
 if(!message.member.permissions.has("KICK_MEMBERS")) return message.channel.send("Hier heb jij **geen** rechten voor!");
 
